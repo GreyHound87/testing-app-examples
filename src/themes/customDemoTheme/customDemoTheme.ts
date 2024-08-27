2a65b0710bed157b3e6c358b6cbd7554dd896cc1
Sergei Smirnov <sasmirnov87@gmail.com>
2024-08-27 22:29:10 +0300

    themes deleted

--- src/themes/customDemoTheme/customDemoTheme.ts
+++
@@ -1,6 +0,0 @@
-import { createTheme } from '@hh.ru/magritte-design-tokens-utils'
-
-export const customDemoTheme = createTheme('v1-0-2', {
-  colorPrimary: '#ff0000',
-  colorSecondary: '#00ff00',
-})
